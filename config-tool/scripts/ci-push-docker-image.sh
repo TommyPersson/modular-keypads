f6d325a7350b3b12775b5b99dac5a2875f1cd25a
Tommy Persson <tommy.persson@gmail.com>
2026-06-28 20:14:39 +0200

    Also publish docker latest tag

--- config-tool/scripts/ci-push-docker-image.sh
+++ config-tool/scripts/ci-push-docker-image.sh
@@ -5,5 +5,8 @@ set -euxo pipefail
 source "${BASH_SOURCE%/*}/script-setup.sh"
 
 docker tag $IMAGE_NAME $IMAGE_NAME:$VERSION
+docker tag $IMAGE_NAME:$VERSION $IMAGE_NAME:latest
+
 docker push $IMAGE_NAME:$VERSION
+docker push $IMAGE_NAME:latest
 
